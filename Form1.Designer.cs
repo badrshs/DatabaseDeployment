e8574cc7650e44b276215284febdec08d5b1e555
badr aldeen shek salim <shs1bader@gmail.com>
2020-05-09 03:23:10 +0300

    Initialize the project

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectionStringTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePathListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ConnectionStringTextBox
+            // 
+            this.ConnectionStringTextBox.Location = new System.Drawing.Point(129, 51);
+            this.ConnectionStringTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ConnectionStringTextBox.Name = "ConnectionStringTextBox";
+            this.ConnectionStringTextBox.Size = new System.Drawing.Size(781, 22);
+            this.ConnectionStringTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(462, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(467, 444);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePathListBox
+            // 
+            this.filePathListBox.FormattingEnabled = true;
+            this.filePathListBox.ItemHeight = 16;
+            this.filePathListBox.Location = new System.Drawing.Point(24, 97);
+            this.filePathListBox.Name = "filePathListBox";
+            this.filePathListBox.Size = new System.Drawing.Size(1017, 340);
+            this.filePathListBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 510);
+            this.Controls.Add(this.filePathListBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ConnectionStringTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Database Deployement";
+            this.Text = "Database Deployement";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ConnectionStringTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox filePathListBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+
+            string contents = File.ReadLines($"{AppDomain.CurrentDomain.BaseDirectory}/importedFiles.txt").FirstOrDefault();
+            ConnectionStringTextBox.Text = contents;
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            filePathListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+
+            filePathListBox.Items.Clear();
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string contents = File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/importedFiles.txt");
+
+            Array.Sort<string>(files);
+            int i = 0;
+            foreach (string file in files)
+            {
+                if (!contents.Contains(file.ToString()))
+                {
+                    filePathListBox.Items.Add(file);
+                }
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (filePathListBox.Items.Count == 0)
+                    return;
+                string sqlConnectionString = ConnectionStringTextBox.Text;
+
+                SqlConnection conn = new SqlConnection(sqlConnectionString);
+                Server server = new Server(new ServerConnection(conn));
+                string contents = File.ReadAllText($"{AppDomain.CurrentDomain.BaseDirectory}/importedFiles.txt");
+                if (contents.Length == 0)
+                {
+                    File.AppendAllLines($"{AppDomain.CurrentDomain.BaseDirectory}/importedFiles.txt", new[] { sqlConnectionString });
+                }
+                else
+                {
+                    File.AppendAllLines($"{AppDomain.CurrentDomain.BaseDirectory}/importedFiles.txt", new[] { "" });
+                }
+
+                foreach (var file in filePathListBox.Items)
+                {
+                    if (!contents.Contains(file.ToString()))
+                    {
+                        server.ConnectionContext.ExecuteNonQuery(File.ReadAllText(file.ToString()));
+                        File.AppendAllLines($"{AppDomain.CurrentDomain.BaseDirectory}/importedFiles.txt", new[] { file.ToString() });
+                    }
+                }
+                MessageBox.Show(filePathListBox.Items.Count + " scripts Deployed Sucessfully", "Successfully Deployed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.InnerException.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
